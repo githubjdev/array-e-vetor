27344f6250ec01da7a8dd1aff17c12aa7d69c537
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-02 09:59:34 -0300

    .

---
+++ array-e-vetor/src/main/java/exemplo01/Ex1.java
@@ -0,0 +1,12 @@
+package exemplo01;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		/*Declaração simples de um array de inteiro*/
+		int[] numeros = { 10, 20, 30, 40 };
+
+	}
+
+}
---
+++ array-e-vetor/src/main/java/exemplo03/Ex3.java
@@ -0,0 +1,13 @@
+package exemplo03;
+
+public class Ex3 {
+	
+	
+	public static void main(String[] args) {
+		
+		/*Declarar com String e direto com valores*/
+		String[] nomes = {"Ana", "Pedro", "Lucas"};
+
+	}
+
+}
