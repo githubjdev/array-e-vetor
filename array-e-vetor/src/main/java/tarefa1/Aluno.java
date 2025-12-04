9b549f1ded9b31a89ecfe4cfcb71322de5702e0b
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-04 09:29:52 -0300

    ..

---
+++ array-e-vetor/src/main/java/tarefa1/Aluno.java
@@ -0,0 +1,61 @@
+package tarefa1;
+
+public class Aluno {
+
+	public String nome;
+
+	double[] notas = new double[4];
+
+	public Aluno(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + "]";
+	}
+
+	public String exibirNota() {
+		String retorno = "";
+		for (double nota : notas) {
+			retorno += "" + nota + "\n";
+		}
+		return retorno;
+	}
+
+	public double somaNota() {
+
+		double somanota = 0;
+
+		for (double nota : getNotas()) {
+
+			somanota += nota;
+
+		}
+		return somanota;
+
+	}
+
+	public double mediaNota() {
+
+		return somaNota() / getNotas().length;
+	}
+
+}
